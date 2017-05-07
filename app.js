cb1285ab02dabf6e8cb0445a2b9e472146c7d050
Arun Seehra <seehra.a@gmail.com>
2017-05-06 21:03:38 -0500

    Ex1

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+
+app.use((req, res, next) => {
+  console.log('Request IP: ' + req.url)
+  console.log('Request date: ' + new Date())
+  next()
+})
+
+app.use((req, res, next) => {
+  const filePath = path.join(__dirname, 'static', req.url)
+  fs.stat(filePath, (err, fileInfo) => {
+    if (err) {
+      next()
+      return
+    }
+
+    if (fileInfo.isFile()) {
+      res.sendFile(filePath)
+      return
+    }
+
+    next()
+  })
+})
+
+app.use((req, res) => {
+  res.status(404)
+  res.send('File not found')
+})
+
+app.listen(3000, () => {
+  console.log('App started on port 3000')
+})
