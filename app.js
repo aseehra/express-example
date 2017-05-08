4671953faf35852364efa092fd4f301a04891cc7
Arun Seehra <seehra.a@gmail.com>
2017-05-08 13:55:15 -0500

    Start fresh

--- app.js
+++ app.js
@@ -1,19 +1,1 @@
-const express = require('express')
-const morgan = require('morgan')
-const path = require('path')
-const apiRouter = require('./routes/apiRouter')
 
-const app = express()
-
-app.use(morgan('short'))
-
-const staticPath = path.resolve(__dirname, 'static')
-app.use(express.static(staticPath))
-
-app.use('/api', apiRouter)
-
-app.use((request, response) => {
-  response.status(400).send('Not found')
-})
-
-app.listen(3000)
