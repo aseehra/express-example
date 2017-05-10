5e325e1b86c53a8fc02b06edcd10c65eb4f9bb77
Arun Seehra <seehra.a@gmail.com>
2017-05-09 20:04:30 -0500

    Add plain-text api tests

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const express = require('express')
 
+const appBuilder = function appBuilder () {
+  const app = express()
+
+  app.get('/', (req, res) => {
+    res.type('text')
+    res.send(req.headers['user-agent'])
+  })
+
+  return app
+}
+
+module.exports = appBuilder
---
+++ test/txt.js
@@ -0,0 +1,27 @@
+import test from 'ava'
+import supertest from 'supertest'
+
+import app from '../app'
+
+let request
+
+test.beforeEach((t) => {
+  request = supertest(app())
+    .get('/')
+    .set('User-Agent', 'my cool browser')
+    .set('Accept', 'text/plain')
+})
+
+test('Plain text API should return a plain text response', (t) => {
+  return request
+    .expect('Content-Type', /text\/plain/)
+    .expect(200)
+    .then(() => t.pass())
+})
+
+test('Plain text API should return the User Agent', (t) => {
+  return request
+    .then((res) => {
+      t.is(res.text, 'my cool browser')
+    })
+})
